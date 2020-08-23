50bdebf75a7c484153af6b5fc43a69a4f19f3ca2
Igor Gutsaev <igor.gutsaev@filuet.ru>
2020-08-23 17:36:56 +0300

    + abstractions nuspec

--- CreateSDK - swagger.bat
+++ CreateSDK - swagger.bat
@@ -1,5 +1,5 @@
-call autorest initialize.md --v3 --static-initializer --add-credentials --input-file=apimatic-swagger.json --csharp --namespace=Filuet.Fusion.SDK --output-folder=./Filuet.Fusion.SDK/Code
-dotnet new classlib --name Filuet.Fusion.SDK --force --framework netstandard2.0
+call autorest initialize.md --v3 --static-initializer --add-credentials --input-file=apimatic-swagger.json --csharp --namespace=Filuet.Fusion.SDK --output-folder=./Filuet.Hrbl.Ordering.SDK/Code
+dotnet new classlib --name Filuet.Hrbl.Ordering.SDK --force --framework netstandard2.0
 
 del Filuet.Fusion.SDK\Class1.cs
 rmdir Filuet.Fusion.SDK\Models /S /Q
@@ -8,4 +8,4 @@ cd Filuet.Fusion.SDK
 dotnet add package Microsoft.Rest.ClientRuntime -v 2.3.18
 
 cd..
-dotnet sln Filuet.Fusion.SDK.sln add Filuet.Fusion.SDK\Filuet.Fusion.SDK.csproj
+dotnet sln Filuet.Fusion.SDK.sln add Filuet.Hrbl.Ordering.SDK\Filuet.Hrbl.Ordering.SDK.csproj
