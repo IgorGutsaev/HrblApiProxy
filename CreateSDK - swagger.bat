ba4b71359955967d777f64ba368090b75f6cecae
Igor Gutsaev <igor.gutsaev@filuet.ru>
2020-08-23 18:24:12 +0300

    renaming

--- CreateSDK - swagger.bat
+++ CreateSDK - swagger.bat
@@ -1,11 +1,12 @@
-call autorest initialize.md --v3 --static-initializer --add-credentials --input-file=apimatic-swagger.json --csharp --namespace=Filuet.Fusion.SDK --output-folder=./Filuet.Hrbl.Ordering.SDK/Code
-dotnet new classlib --name Filuet.Hrbl.Ordering.SDK --force --framework netstandard2.0
+call autorest initialize.md --v3 --static-initializer --add-credentials --input-file=apimatic-swagger.json --csharp --namespace=Filuet.Fusion.SDK --output-folder=./SDK/Code
+dotnet new classlib -o SDK --name Filuet.Hrbl.Ordering.SDK --force --framework netstandard2.0
 
-del Filuet.Fusion.SDK\Class1.cs
-rmdir Filuet.Fusion.SDK\Models /S /Q
+del SDK\Class1.cs
+rmdir SDK\Models /S /Q
 
-cd Filuet.Fusion.SDK
+cd SDK
 dotnet add package Microsoft.Rest.ClientRuntime -v 2.3.18
+dotnet add package Newtonsoft.Json -v 12.0.3
 
 cd..
-dotnet sln Filuet.Fusion.SDK.sln add Filuet.Hrbl.Ordering.SDK\Filuet.Hrbl.Ordering.SDK.csproj
+dotnet sln Filuet.Hrbl.Ordering.SDK.sln add SDK\Filuet.Hrbl.Ordering.SDK.csproj
