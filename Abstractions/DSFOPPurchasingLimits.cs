85428f7bcc317cfde4497e49d30dde45d4460910
Igor Gutsaev <igor.gutsaev@filuet.ru>
2020-08-24 18:16:43 +0300

    FOPPurchasingLimitsResult dto

--- Abstractions/DSFOPPurchasingLimits.cs
+++ Abstractions/DSFOPPurchasingLimits.cs
@@ -1,10 +1,91 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Filuet.Hrbl.Ordering.Abstractions
 {
-    public class FOPPurchasingLimits
+    public class FOPPurchasingLimitsResult
     {
+        [JsonProperty("DSFOPLimits")]
+        private DSFOPLimits DSFOPLimits { get; set; }
+
+        [JsonIgnore]
+        public DSFOPLimit FopLimit => DSFOPLimits.DSFOPLimit;
+
+        [JsonProperty("DSOTLimits")]
+        public string DSOTLimits { get; private set; }
+    }
+
+    public class DSFOPLimits
+    {
+        [JsonProperty("DSFOPLimit")]
+        public DSFOPLimit DSFOPLimit { get; private set; }
+    }
+
+    public class DSFOPLimit
+    {
+        [JsonProperty("EarnedFOP")]
+        public string EarnedFOP { get; private set; }
+
+        [JsonProperty("AvailableFOPLimit")]
+        public decimal? AvailableFOPLimit { get; private set; }
+
+        [JsonProperty("ThresholdFOPimit")]
+        public decimal? ThresholdFOPimit { get; private set; }
+
+        [JsonProperty("FOPFirstOrderDate")]
+        public DateTime? FOPFirstOrderDate { get; private set; }
+
+        [JsonProperty("FOPThresholdPeriod")]
+        public int FOPThresholdPeriod { get; private set; }
+    }
+
+    public class DSPurchasingLimits
+    {
+        [JsonProperty("DSPurchasingLimit")]
+        public DSPurchasingLimit[] DSPurchasingLimit { get; set; }
+    }
+
+    public class DSPurchasingLimit
+    {
+        [JsonProperty("PPVOrderMonth")]
+        public string PPVOrderMonth { get; private set; }
+
+        [JsonProperty("EarnedPC")]
+        public decimal EarnedPC { get; private set; }
+
+        [JsonProperty("AvailablePCLimit")]
+        public decimal AvailablePCLimit { get; private set; }
+
+        [JsonProperty("ThresholdPCLimit")]
+        public decimal ThresholdPCLimit { get; private set; }
+
+        [JsonProperty("EarnedAI")]
+        public decimal EarnedAI { get; private set; }
+
+        [JsonProperty("AvailableAILimit")]
+        public decimal AvailableAILimit { get; private set; }
+
+        [JsonProperty("ThresholdAILimit")]
+        public decimal ThresholdAILimit { get; private set; }
+
+        [JsonProperty("EarnedRO")]
+        public decimal EarnedRO { get; private set; }
+
+        [JsonProperty("AvailableROLimit")]
+        public decimal AvailableROLimit { get; private set; }
+
+        [JsonProperty("ThresholdROLimit")]
+        public decimal ThresholdROLimit { get; private set; }
+
+        [JsonProperty("EarnedCD")]
+        public decimal EarnedCD { get; private set; }
+
+        [JsonProperty("AvailableCDLimit")]
+        public decimal AvailableCDLimit { get; private set; }
+
+        [JsonProperty("ThresholdCDLimit")]
+        public decimal ThresholdCDLimit { get; private set; }
     }
 }
