3e1c025c1245afaafa6e1fba4195a85df9f2c426
Igor <igor.gutsaev@filuet.ru>
2021-03-31 15:39:50 +0300

    Dates&Flags: + Can't buy boolean property

--- Abstractions/DistributorProfile.cs
+++ Abstractions/DistributorProfile.cs
@@ -722,6 +722,9 @@ namespace Filuet.Hrbl.Ordering.Abstractions
 
         [JsonProperty("DataPriv")]
         public string DataPriv { get; private set; }
+
+        [JsonIgnore]
+        public bool IsCantBuy => CantBuy.Equals("y", StringComparison.InvariantCultureIgnoreCase);
     }
 
     public sealed class DistTinsMessage
