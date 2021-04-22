34631a1288640e17efc331b839e05e1163b23b63
Igor <igor.gutsaev@filuet.ru>
2021-04-22 13:01:33 +0300

    +InventoryItemDto

---
+++ Abstractions/Dto/InventoryItemDto.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Filuet.Hrbl.Ordering.Abstractions.Dto
+{
+    public class InventoryItemDto
+    {
+        [JsonProperty("sku")]
+        public string Sku { get; private set; }
+
+        [JsonProperty("wh")]
+        public string Warehouse { get; private set; }
+
+        [JsonProperty("block")]
+        public bool IsBlocked { get; private set; }
+
+        [JsonProperty("qty")]
+        public uint QuantityAvailable { get; private set; }
+
+        public static InventoryItemDto Create(InventoryItem item)
+            => new InventoryItemDto {
+                Sku = item.SKU,
+                QuantityAvailable = (uint)item.QuantityAvailable,
+                Warehouse = item.Warehouse,
+                IsBlocked = item.IsBlocked
+            };
+    }
+}
